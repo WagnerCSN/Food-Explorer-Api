09e889c1701eaafabf548c605dd4eb1d0f6cfc9a
WagnerCSN <wagcostvez@gmail.com>
2023-08-19 20:27:58 -0300

    exception handling

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
+require("express-async-error");
+const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.statusCode(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  return response.statusCode(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const Port = 3000;
 app.listen(Port, () => {
---
+++ src/utils/AppError.js
@@ -0,0 +1,10 @@
+class AppError {
+  message;
+  statusCode;
+  constructor(message, statusCode) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = AppError;
