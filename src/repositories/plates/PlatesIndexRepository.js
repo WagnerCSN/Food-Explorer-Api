fad0ccd0c7c64e503a90f77b9f834e47955bd760
WagnerCSN <wagcostvez@gmail.com>
2023-09-26 22:39:21 -0300

    update

--- src/repositories/plates/PlatesIndexRepository.js
+++ src/repositories/plates/PlatesIndexRepository.js
@@ -14,9 +14,16 @@ class PlatesIndexRepository{
     }
 
     async indexByTypeOfPlates(typeOfPlate_id){
-        const platesIndexTypeOfPlate_id = await knex("plates").whereLike("typeOfPlate_id", `%${typeOfPlate_id}%`);
+        // const plates = await knex("plates").select(["id", "name", "description", "cost", "image"]).where({id}).first();
+        const platesIndexTypeOfPlate_id = await knex("plates").select(["typeOfPlates.name"]).where("typeOfPlates.id",typeOfPlate_id).whereLike("typeOfPlate_id", `%${typeOfPlate_id}%`).innerJoin("typeOfPlates", "typeOfPlate_id", "plates.typeOfPlate_id");
 
-        return platesIndexTypeOfPlate_id;
+        const platesWithType = platesIndexTypeOfPlate_id.map(TypeOfPlate =>{
+            return {
+                    // ...plates,
+                    "tipo de plato": TypeOfPlate.name
+            }
+        })
+        return platesWithType;
     }
 
     async indexByIngredients(ingredient_id){
--- src/services/plates/PlatesIndexService.js
+++ src/services/plates/PlatesIndexService.js
@@ -1,17 +1,34 @@
 const AppError = require("../../utils/AppError");
+const knex = require("../../database/knex");
 
 class PlatesIndexService{
     constructor(platesIndexRepository){
         this.platesIndexRepository = platesIndexRepository;
     }
 
-    async execute({name, cost, typeOfPlate_id, ingredient_id}){
+    async execute({id, name, cost, typeOfPlate_id, ingredient_id}){
+        let plates;
         if(name){
-            const platesIndexName = await this.platesIndexRepository.indexByName(name);
-            if(platesIndexName.length ===0){
-                throw new AppError("Enter a valid name!");
+            // const platesIndexName = await this.platesIndexRepository.indexByName(name);
+            // if(platesIndexName.length ===0){
+            //     throw new AppError("Enter a valid name!");
+            // }
+
+            
+           const filter = name.split(",").map(nome=>nome.trim());
+             plates = await knex("typeOfPlates").select().innerJoin("plates", "plates.typeOfPlate_id", "typeOfPlates.id").whereLike("typeOfPlates.name", `%${name}%`); //whereIn("typeOfPlates.name", filter).where("plates.id",id).whereLike("plates.name", `%${name}%`);
+          
+            const typeid = await knex("typeofplates").select();
+            const plateswithtype = plates.map(plate => {
+                const platetype = typeid.filter(type => type.id ===plate.typeOfPlate_id);
+                
+            return{
+                ...plate,
+                platetype
             }
-            return platesIndexName;
+            });
+           
+             return plateswithtype;
         }
             
         if(cost){
@@ -22,13 +39,17 @@ class PlatesIndexService{
             return platesIndexCost;
         }
 
-        if(typeOfPlate_id){
-            const platesIndexTypeOfPlate = await this.platesIndexRepository.indexByTypeOfPlates(typeOfPlate_id);
-            if(platesIndexTypeOfPlate.length ===0){
-                throw new AppError("Enter a valid type Of Plate!");
-            }
-            return platesIndexTypeOfPlate;
-        }
+        // if(typeOfPlate_id){
+        //     const platesIndexTypeOfPlate = await this.platesIndexRepository.indexByTypeOfPlates(typeOfPlate_id);
+           
+        //     if(platesIndexTypeOfPlate.length ===0){
+        //         throw new AppError("Enter a valid type Of Plate!");
+        //     }
+
+ 
+
+        //     return platesIndexTypeOfPlate;
+        // }
 
         if(ingredient_id){
             const platesIndexIngredient = await this.platesIndexRepository.indexByIngredients(ingredient_id);
