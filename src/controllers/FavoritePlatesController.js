8b0c3116fe228c019adefd5a14fed06688c21cc0
WagnerCSN <wagcostvez@gmail.com>
2023-09-28 23:05:18 -0300

    update

--- src/controllers/FavoritePlatesController.js
+++ src/controllers/FavoritePlatesController.js
@@ -1,38 +1,38 @@
+const FavoritePlatesRepository = require("../repositories/favoritePlates/FavoritePlatesRepository");
+const FavoritePlatesCreateService = require("../services/favoritePlates/FavoritePlatesCreateService");
+const FavoritePlatesDeleteRepository = require("../repositories/favoritePlates/FavoritePlatesDeleteRepository");
+const FavoritePlatesDeleteService = require("../services/favoritePlates/FavoritePlatesDeleteService");
+const FavoritePlatesIndexRepository = require("../repositories/favoritePlates/FavoritePlatesIndexRepository");
+const FavoritePlatesIndexService = require("../services/favoritePlates/FavoritePlatesIndexService");
+
 class FavoritePlatesController{
     async create(request, response) {
         const { favorite } = request.body;
-
-        favoritePlatesReporitory = new FavoritePlatesRepository();
-        favoritePlatesCreateService = new FavoritePlatesCreateService(favoritePlatesReporitory);
-        await favoritePlatesCreateService.execute({favorite})
+        const { client_id, plate_id} = request.query;
+        const favoritePlatesReporitory = new FavoritePlatesRepository();
+        const favoritePlatesCreateService = new FavoritePlatesCreateService(favoritePlatesReporitory);
+        await favoritePlatesCreateService.execute({favorite, client_id, plate_id})
 
         response.json();
 
     }
+
+    async index(request, response){
+      const { name_user, name_plates, favorite} = request.query;
   
-    async update(request, response) {
-        const { name, email, password, old_password } = request.body;
-        // const id = request.user.id;
-        const { id } = request.params;
-        const usersUpdateRepository = new UsersUpdateRepository();
-        const usersUpdateService = new UsersUpdateService(usersUpdateRepository);
-        await usersUpdateService.execute({
-          name,
-          email,
-          password,
-          old_password,
-          id,
-        });
-    
-        response.json();
-      }
-      
-      async delete(request, response){
+      const favoritePlatesIndexRepository = new FavoritePlatesIndexRepository();
+      const favoritePlatesIndexService = new FavoritePlatesIndexService(favoritePlatesIndexRepository);
+      const favoritePlatesSearch = await favoritePlatesIndexService.execute({name_user, name_plates, favorite});
+      response.json(favoritePlatesSearch);
+  
+    }
+  
+    async delete(request, response){
         const {id} = request.params;
     
-        const usersDeleteRepository = new UsersDeleteRepository();
-        const usersDeleteService = new UsersDeleteService(usersDeleteRepository);
-        await usersDeleteService.execute({id});
+        const favoritePlatesDeleteRepository = new FavoritePlatesDeleteRepository();
+        const favoritePlatesDeleteService = new FavoritePlatesDeleteService(favoritePlatesDeleteRepository);
+        await favoritePlatesDeleteService.execute({id});
     
         return response.json();
       }
