98c052585ee934aa98eba10667da8e80a6eeb0d4
WagnerCSN <wagcostvez@gmail.com>
2023-10-01 19:06:31 -0300

    update

--- src/repositories/blog/BlogIndexRepository.js
+++ src/repositories/blog/BlogIndexRepository.js
@@ -8,21 +8,10 @@ class BlogIndexRepository{
     }
 
     async selectByPlate(plate_name){
-        let plate = await knex("blog").select(['plates.id', 'plates.name']).innerJoin("plates", "blog.plate_id", "plates.id").whereLike("plates.name", `%${plate_name}%`).orderBy("created_at"); 
-        const blogs = await knex("blog").select();
-     
-        const plates = plate.map(plate => {
-            const blogWithPlate = blogs.filter(blog => blog.plate_id===plate.id)
+       
+         const plate = await knex("blog").select('blog.id',  'blog.name', 'blog.comments', 'blog.rating').innerJoin("plates", "blog.plate_id", "plates.id").whereLike("plates.name", `%${plate_name}%`).orderBy("created_at"); 
         
-            return{
-                namePlato: plate.name,
-                name: blogWithPlate.map(blog => blog.name).toString(),
-                comments: blogWithPlate.map(blog => blog.comments).toString(),
-                rating: blogWithPlate.map(blog => blog.rating).toString(),
-                date: blogWithPlate.map(blog => blog.created_at).toString()  
-            }
-        });
-        return plates;
+         return plate;
     }
 
 }
