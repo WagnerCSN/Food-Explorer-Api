631024bb00fbf183166019c50cc9d784fde8b90a
WagnerCSN <wagcostvez@gmail.com>
2023-12-28 21:39:26 -0200

    update

--- src/services/order/OrderDeleteService.js
+++ src/services/order/OrderDeleteService.js
@@ -7,16 +7,15 @@ class OrderDeleteService{
 
     async execute({id}){
         const checkOrderExist = await this.orderDeleteRepository.findByOrder(id);
+        console.log(checkOrderExist)
 
         if(!checkOrderExist){
             throw new AppError("This order does not have!");
         }
 
-        const deletedOrder = await this.orderDeleteRepository.deleteOrder(id);
+        await this.orderDeleteRepository.deleteOrder(id);
 
-        if(deletedOrder){
-            throw new AppError("Successfully deleted");
-        }
+        
 
     }
 }
