793d25954e9ff8eddf6c2abcb1d499f2e2ef8c36
WagnerCSN <wagcostvez@gmail.com>
2023-12-31 16:58:56 -0200

    update

--- src/server.js
+++ src/server.js
@@ -14,12 +14,20 @@ app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(cookieParser());
 
-app.use(cors({
-  origin: '*',
-  methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  headers: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-  credentials:true
-}));
+const corsOptions = {
+  credentials: true,
+  origin: 'https://foodexplorer-wagner.netlify.app'
+}
+
+app.use(cors(corsOptions));
+
+// app.use(cors({
+//   origin: 'https://foodexplorer-wagner.netlify.app/',
+//   methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//   headers: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
+//   credentials:true
+// }));
+
 app.use(routes);
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
