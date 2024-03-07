8f21cb632c6db8d6a589e104e02ee4dee89db907
WagnerCSN <wagcostvez@gmail.com>
2024-03-06 22:57:49 -0300

    update

--- src/services/checkoutSession/CheckoutSessionCreateService.js
+++ src/services/checkoutSession/CheckoutSessionCreateService.js
@@ -14,7 +14,7 @@ class CheckoutSessionCreateService{
       metadata: {
         userId: user_id.toString(),
         cart: JSON.stringify(cartItems.toString()),
-        orderId: JSON.stringify(order_id.id.toString())
+        orderId: order_id.id
       }
     })
 
